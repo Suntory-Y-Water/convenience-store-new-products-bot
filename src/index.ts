eec59e7c631cc3b3029f3da9872ea207210460f3
sui <1202ayasnppk@gmail.com>
2023-12-23 00:10:22 +0900

    add: test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { Attachment, writeToSpreadsheet } from './services/spreadsheetUtils';
+import { extractField, getMatch } from './utils/stringUtils';
+
 const REGION = '関東';
 const REGIONS: { [key: string]: string } = {
   北海道: 'hokkaido',
@@ -20,19 +23,37 @@ const SEVEN_COLOR = [
   '#ee1c23', // セブンレッド
 ];
 
-interface Attachment {
-  color?: string;
-  title: string;
-  title_link: string;
-  thumb_url: string;
-  fields: Array<{ title: string; value: string; short: boolean }>;
+/**
+ * 商品の詳細を抽出する
+ * @param {string} itemHtml
+ * @return {*}  {Attachment}
+ */
+function extractItemDetails(itemHtml: string): Attachment {
+  const link = getMatch(itemHtml, /<a href="([^"]+)"/) || '';
+  const image = getMatch(itemHtml, /data-original="([^"]+)"/) || '';
+  const name =
+    getMatch(itemHtml, /<div class="item_ttl"><p><a href="[^"]+">([^<]+)<\/a><\/p><\/div>/) || '';
+
+  return {
+    title_link: ORIGIN + link,
+    thumb_url: image,
+    title: name,
+    fields: [
+      extractField(itemHtml, /<div class="item_price"><p>([^<]+)<\/p><\/div>/, '値段'),
+      extractField(itemHtml, /<div class="item_launch"><p>([^<]+)<\/p><\/div>/, '販売時期'),
+      extractField(
+        itemHtml,
+        /<div class="item_region"><p><span>販売地域：<\/span>([^<]+)<\/p><\/div>/,
+        '販売地域',
+      ),
+    ],
+  };
 }
 
-function getMatch(input: string, regex: RegExp): string | null {
-  const match = input.match(regex);
-  return match ? match[1] : null;
-}
-
+/**
+ * メイン処理
+ * @return {*}  {void}
+ */
 function main(): void {
   const attachments: Attachment[] = [];
 
@@ -57,61 +78,4 @@ function main(): void {
   writeToSpreadsheet(attachments);
 }
 
-function extractField(
-  itemHtml: string,
-  regex: RegExp,
-  fieldTitle: string,
-): { title: string; value: string; short: boolean } {
-  const value = getMatch(itemHtml, regex) || '';
-  return { title: fieldTitle, value: value, short: true };
-}
-
-function extractItemDetails(itemHtml: string): Attachment {
-  const link = getMatch(itemHtml, /<a href="([^"]+)"/) || '';
-  const image = getMatch(itemHtml, /data-original="([^"]+)"/) || '';
-  const name =
-    getMatch(itemHtml, /<div class="item_ttl"><p><a href="[^"]+">([^<]+)<\/a><\/p><\/div>/) || '';
-
-  return {
-    title_link: ORIGIN + link,
-    thumb_url: image,
-    title: name,
-    fields: [
-      extractField(itemHtml, /<div class="item_price"><p>([^<]+)<\/p><\/div>/, '値段'),
-      extractField(itemHtml, /<div class="item_launch"><p>([^<]+)<\/p><\/div>/, '販売時期'),
-      extractField(
-        itemHtml,
-        /<div class="item_region"><p><span>販売地域：<\/span>([^<]+)<\/p><\/div>/,
-        '販売地域',
-      ),
-    ],
-  };
-}
-
-function writeToSpreadsheet(data: Attachment[]): void {
-  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  const sheet = spreadsheet.getSheetByName('シート1'); // スプレッドシートのシート名を適宜設定
-
-  if (!sheet) {
-    throw new Error('シートがありません.');
-  }
-
-  // 既存のデータをクリア
-  sheet.clear();
-
-  // ヘッダーの追加
-  sheet.appendRow(['商品名', '値段', '販売時期', '販売地域', 'リンク', '画像URL']);
-
-  // 新しいデータを書き込み
-  data.forEach((item) => {
-    const row = [
-      item.title,
-      item.fields.find((f) => f.title === '値段')?.value || '',
-      item.fields.find((f) => f.title === '販売時期')?.value || '',
-      item.fields.find((f) => f.title === '販売地域')?.value || '',
-      item.title_link,
-      item.thumb_url,
-    ];
-    sheet.appendRow(row);
-  });
-}
+main();
